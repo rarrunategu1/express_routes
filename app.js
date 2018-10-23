0e113adddc0a58e61ac63f9936691e7c9297acd3
Rose Parker <parker.rose@ymail.com>
2018-10-23 00:46:38 +0000

    added dynamic get route that gets name param and send a string with the user name to welcome them

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var parseUrlencoded = bodyParser.urlencoded({extended: false });
 
 
 app.get('/', function(req, res){
     res.send('Rose Parker');
 });
 
+app.get('/user/:name', function(req, res) {
+    res.send('Welcome ' + req.params.name + '!');
+});
 
 
 app.listen(process.env.PORT, process.env.IP, 8080, function() {
