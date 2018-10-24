3fa2632b31c861676dc47f2c5eecb2fc644145a6
Rose Parker <parker.rose@ymail.com>
2018-10-24 00:33:30 +0000

    added route to get 2 query params, multiply and return the product to the user

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var parseUrlencoded = bodyParser.urlencoded({extended: false });
+var queryParser = require('express-query-int');
+
+app.use(bodyParser.json());
+app.use(queryParser({
+    parser: parseFloat
+}));
 
 
 app.get('/', function(req, res){
@@ -12,6 +18,11 @@ app.get('/user/:name', function(req, res) {
     res.send('Welcome ' + req.params.name + '!');
 });
 
+app.get('/user', function(req, res) {
+    var queryParam = req.query;
+   res.send('The product of ' + queryParam.numone +' and ' +queryParam.numtwo + ' is ' + parseInt(queryParam.numone, 10) * parseInt(queryParam.numtwo, 10) + "!");
+  
+});
 
 app.listen(process.env.PORT, process.env.IP, 8080, function() {
     console.log('Listening on port 8080');
